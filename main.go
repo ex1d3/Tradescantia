b052a8a3308377caecb0ee816b1a54cd793fe5ce
artur <okolobaxha@yandex.ru>
2024-03-22 22:22:08 +0700

    feat: connected to main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"tradescantia/cli"
+	"tradescantia/config"
+	"tradescantia/cryptomus"
+)
+
+func main() {
+	cfg, err := config.Parse()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cliArgs := cli.ParseArgs()
+	cliArgs.Validate()
+
+	webhookDto := cryptomus.TestWebhookDto{
+		UrlCallback: cliArgs.CallbackUrl,
+		Currency:    "USDT",
+		Network:     "TRON",
+		UUID:        cliArgs.InvoiceUUID,
+		Status:      "paid",
+	}
+
+	httpClient := http.Client{}
+
+	cryptomusClient := cryptomus.New(&httpClient, cfg.MerchantId, cfg.ApiKey)
+	cryptomusClient.TestWebhook(webhookDto)
+}
